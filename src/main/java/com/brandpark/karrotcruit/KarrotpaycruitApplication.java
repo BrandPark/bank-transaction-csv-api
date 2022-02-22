65d3360fafa39ddafb21962914cbc2c28747724e
brandpark <pak2005@hanmail.net>
2022-02-22 18:31:19 +0900

    ㅑinit project

---
+++ src/main/java/com/brandpark/karrotcruit/KarrotpaycruitApplication.java
@@ -0,0 +1,13 @@
+package com.brandpark.karrotcruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarrotpaycruitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarrotpaycruitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandpark/karrotcruit/KarrotpaycruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandpark.karrotcruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarrotpaycruitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
